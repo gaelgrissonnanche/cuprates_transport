b492c6fcfc0b8f9a1c23aa4f9d8872197123c32c
Gael Grissonnanche <gael.phys@gmail.com>
2018-11-27 17:37:42 -0500

    Small corrections to main

--- main_AF.py
+++ main_AF.py
@@ -6,7 +6,7 @@ from admr import ADMR
 from chambers import Conductivity
 ##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#
 
-bandObject = HolePocket(mu = -0.78, AFgap = 0.1)
+bandObject = HolePocket(mu = -0.78, AFgap = 10, mesh_ds = pi / 15, numberOfKz = 7)
 bandObject.discretize_FS()
 bandObject.densityOfState()
 bandObject.doping()
@@ -14,41 +14,18 @@ bandObject.doping()
 # print(bandObject.mu)
 # bandObject.discretize_FS()
 # bandObject.figDiscretizeFS2D()
-# bandObject.figMultipleFS2D()
+bandObject.figMultipleFS2D()
 
-# condObject = Conductivity(bandObject, Bamp=45, Bphi=0, Btheta=0)
+# condObject = Conductivity(bandObject, Bamp = 45, Bphi = 0, Btheta = 0, gamma_0=1, gamma_k=0, power=12, a0=0)
+# condObject.solveMovementFunc()
+# condObject.figOnekft(index_kf = 0)
 
-start_total_time = time.time()
-ADMRObject = ADMR(bandObject, Bamp=45, gamma_0=25, gamma_k=0, power=12, a0=0)
-ADMRObject.runADMR()
-print("ADMR time : %.6s seconds" % (time.time() - start_total_time))
+# start_total_time = time.time()
+# ADMRObject = ADMR(bandObject, Bamp=45, gamma_0=25, gamma_k=0, power=12, a0=0)
+# ADMRObject.runADMR()
+# print("ADMR time : %.6s seconds" % (time.time() - start_total_time))
 
-ADMRObject.fileADMR()
-ADMRObject.figADMR()
-
-
-
-# mu_a = np.arange(0.81, 0.835, 0.001)
-# for mu in mu_a:
-#     mu = mu * t
-#     print("mu = " + r"{0:.4f}".format(mu) + " * t" )
-#     band_parameters = np.array([a, b, c, mu, t, tp, tpp, tz, tz2], dtype = np.float64)
-#     admrFunc(band_parameters, mesh_parameters, tau_parameters, B_amp, B_phi_a, B_theta_a, fig_show = False)
-
-# gamma_k_a = np.arange(0, 120, 20)
-# gamma_0_a = np.arange(50, 170, 20)
-# for gamma_0 in gamma_0_a:
-#     for gamma_k in gamma_k_a:
-#         print("gamma_0 = " + r"{0:.1f}".format(gamma_0) + " THz")
-#         print("gamma_k = " + r"{0:.1f}".format(gamma_k) + " THz")
-#         tau_parameters = np.array([gamma_0, gamma_k, power], dtype = np.float64)
-#         admrFunc(band_parameters, mesh_parameters, tau_parameters, B_amp, B_phi_a, B_theta_a, fig_show = False)
-
-
-
-
-
-
-
+# ADMRObject.fileADMR()
+# ADMRObject.figADMR()
 
 
