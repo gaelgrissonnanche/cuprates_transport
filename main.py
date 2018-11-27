ab333d074eaba69abedc303ce30264f568a2e398
Gael Grissonnanche <gael.phys@gmail.com>
2018-11-26 23:25:22 -0500

    Small changes in main

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from admr import ADMR
 from chambers import Conductivity
 ##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#
 
-bandObject = BandStructure(mu = -0.9)
-bandObject.figMultipleFS2D()
-# condObject = Conductivity(bandObject, Bamp=45, Bphi=0, Btheta=0)
+bandObject = BandStructure(mu = -0.825)
+# bandObject.figMultipleFS2D()
 
-# start_total_time = time.time()
-# ADMRObject = ADMR(bandObject, Bamp=45, gamma_0=15, gamma_k=0, power=12, a0=0)
-# ADMRObject.runADMR()
-# print("ADMR time : %.6s seconds" % (time.time() - start_total_time))
 
-# ADMRObject.fileADMR()
-# ADMRObject.figADMR()
+start_total_time = time.time()
+ADMRObject = ADMR(bandObject, Bamp=45, gamma_0=15, gamma_k=65, power=12, a0=0)
+ADMRObject.runADMR()
+print("ADMR time : %.6s seconds" % (time.time() - start_total_time))
+
+ADMRObject.fileADMR()
+ADMRObject.figADMR()
 
 
 
